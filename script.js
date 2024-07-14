8807da2d4475ee2b216f93df6a56ba9c6c194bce
mei higashi <mei@meihigashi.com>
2024-07-13 17:31:41 -0700

    oh no i did smt bad

--- script.js
+++ script.js
@@ -48,11 +48,3 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     loadComments();
 });
-
-const config = {
-    SUPABASE_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4YmZtY21zbW9rbmxkaW5zZGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAyNTI3NDIsImV4cCI6MjAzNTgyODc0Mn0.u_XiHTSZeRYz0k2vlE3um4FhWBsqGR-uwg4aeDl-4VU',
-    SUPABASE_URL: 'https://uxbfmcmsmoknldinsdan.supabase.co'
-};
-
-// Initialize Supabase
-const supabase = createClient(config.SUPABASE_URL, config.SUPABASE_API_KEY);
